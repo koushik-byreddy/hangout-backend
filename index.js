6e3c2425e937251af1c9fda384a52a8702a59d34
koushikreddy-byreddy <koushikreddybyreddy1@gmail.com>
2022-11-23 00:20:02 +0530

    connect frontend

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//cors
+const cors = require("cors");
+
 // Importing express module
 const express = require("express");
 
@@ -12,12 +15,23 @@ const mongoose = require("mongoose");
 
 var app = express();
 app.use(express.json());
-app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 // Creating session
+//session.cookie.secure should be true for htttps onlhy
 app.use(
   session({
     name: "session-id",
     secret: "GFGEnter", // Secret key,
+    cookie: {
+      maxAge: 86400 * 60 * 1000,
+      sameSite: "none",
+    },
     saveUninitialized: false,
     resave: false,
     store: new filestore(),
